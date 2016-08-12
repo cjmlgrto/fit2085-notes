407330235a07783b3f8ec04cb376cf829e3e1c7c
CJ Melegrito <cj@mlgrto.com>
2016-08-12 16:16:50 +1000

    Update heap sort

--- algorithms/18-heap_array.py
+++ algorithms/18-heap_array.py
@@ -6,31 +6,27 @@ class Heap:
     def __len__(self):
         return self.count
 
-    # best case: O(1), when item is smaller or equal to parent
-    # worst case: O(logN), when item has to rise all the way to the top
+    def __str__(self):
+        return str(self.array)
+
     def add(self, item):
-        if self.count + 1 < len(self.array):
-            self.array[self.count+1] = item
-        else:
-            self.array.append(item)
+        self.array.append(item)
         self.count += 1
         self.rise(self.count)
 
     def rise(self, k):
-        while k > 1 and self.array[k] > self.array[k//2]:
+        while self.array[k] > self.array[k//2] and k > 1:
             self.swap(k, k//2)
             k //= 2
 
     def swap(self, i, j):
         self.array[i], self.array[j] = self.array[j], self.array[i]
 
-    # best case: O(1), when item is larger or equal to largest children
-    # worst case: O(log N), when the item sinks all the way to the bottom
     def get_max(self):
-        assert self.count > 0, "Heap is empty!"
-        maximum = self.array[0]
-        self.swap(0, self.count)
-        self.sink(0)
+        self.swap(1, self.count)
+        maximum = self.array.pop(self.count)
+        self.count -= 1
+        self.sink(1)
         return maximum
 
     def sink(self, k):
--- algorithms/18-heap_sort.py
+++ algorithms/18-heap_sort.py
@@ -1,12 +1,10 @@
 from heap import Heap
 
 def heap_sort(L):
-    heap = Heap()
-    new_list = []
-    i = 0
-    while len(heap) < len(L):
-        heap.add(L[i])
-        i += 1
-    while len(heap) > 0:
-        new_list.apppend(heap.get_max())
-    return new_list
+	heap = Heap()
+	new = []
+	for item in L:
+		heap.add(item)
+	for item in L:
+		new.append(heap.get_max())
+	return new
